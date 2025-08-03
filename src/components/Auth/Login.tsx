7c8a3ed74c06d3c5950434c725b39424224da963
Mahmoud Sherif <mahmoudsherifm94@google.com>
2025-08-03 14:28:22 +0300

    Fix build error: Add missing LogIn icon to Sign In button

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -239,7 +239,10 @@ const Login: React.FC = () => {
                       Signing in...
                     </span>
                   ) : (
-                    'Sign In'
+                    <span className="flex items-center justify-center">
+                      <LogIn className="w-5 h-5 mr-2" />
+                      Sign In
+                    </span>
                   )}
                 </button>
               </form>
